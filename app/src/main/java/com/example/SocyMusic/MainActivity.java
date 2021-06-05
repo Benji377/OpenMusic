35c0a75535dae5b249102396db5ba6b413b92930
Benji377 <demetzbenjamin23@gmail.com>
2021-06-05 12:26:08 +0200

    Added functionality for credits menuoption --> popup

--- app/src/main/java/com/example/SocyMusic/MainActivity.java
+++ app/src/main/java/com/example/SocyMusic/MainActivity.java
@@ -9,15 +9,20 @@ import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -116,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements PlayerFragment.On
     }
 
     @Override
+    // Is the option menu you see in the top left corner (3 dots)
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.main, menu);
@@ -123,13 +129,18 @@ public class MainActivity extends AppCompatActivity implements PlayerFragment.On
     }
 
     @Override
+    // Creates the options available and what happens if you click on them
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
+        // To add an item to the menu, add it to menu/main.xml first!
+
         if (item.getItemId() == R.id.credits) {
-            // Replace this
-            Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show();
+            onButtonShowPopupWindowClick(listView);
         } else if (item.getItemId() == R.id.download) {
-            // Replace this
+            // Replace this action
+            Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.settings) {
+            // Replace this action
             Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
@@ -265,6 +276,35 @@ public class MainActivity extends AppCompatActivity implements PlayerFragment.On
         // Add as notification
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    // Shows a pop up window
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window token
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        popupView.setElevation(20);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
 }
