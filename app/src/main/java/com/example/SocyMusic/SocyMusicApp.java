1ec61c07d026dbec4c8a17cd327acfacf5f1dc40
Benji377 <demetzbenjamin23@gmail.com>
2021-06-13 19:45:31 +0200

    Added comments for the methods in the SocyMusicApp class

--- app/src/main/java/com/example/SocyMusic/SocyMusicApp.java
+++ app/src/main/java/com/example/SocyMusic/SocyMusicApp.java
@@ -6,16 +6,28 @@ import android.app.NotificationManager;
 import android.os.Build;
 import com.example.musicplayer.R;
 
+/**
+ * This class is necessary to create notifications according to different Android versions
+ */
 public class SocyMusicApp extends Application {
+    // Unique ID for the media channel only!
     public static final String MEDIA_CHANNEL_ID = "media_channel";
 
+    /**
+     * When this class gets invoked, this method gets called
+     */
     @Override
     public void onCreate() {
         super.onCreate();
         createNotificationChannels();
     }
 
+    /**
+     * According to which version of Android is being used it is necessary to create a
+     * NotificationChannel.
+     */
     private void createNotificationChannels() {
+        // Checks for Buildversion == Androidversion
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel1 = new NotificationChannel(
                     MEDIA_CHANNEL_ID,
