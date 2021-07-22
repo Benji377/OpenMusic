143765dbf4c3a005e59657fb08e4a1f4918d142e
Benji377 <demetzbenjamin23@gmail.com>
2021-07-22 20:43:53 +0200

    Update Playlist.java

--- app/src/main/java/com/musicplayer/SocyMusic/Playlist.java
+++ app/src/main/java/com/musicplayer/SocyMusic/Playlist.java
@@ -10,19 +10,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
-    Since I am struggling a lot with this, I'm just going to write down how it should work and try to
-    figure it out later maybe.
-    So a Playlist is generally a M3U file and an M3U file is almost like a text file which contains
-    all the relative paths to the songs of a playlist.
-    This means that we need a way for the user to create a playlist with a name. This will also be the
-    name of the M3U file. Then when the user wants to add a song to a playlist, he can choose to create
-    a new one, or add it to the existing one. The existing playlist will the add an entry to the M3U file
-    with the location of the song file.
+For devs, this is the M§U file structure, please follow this carefully to avoid issues:
+- The first line defines the type of file
+- The next line pairs are as follow:
+    - The keyword #EXTINF: followed by the information about a song
+    - The path to the song
+Here is an example:
 
-    Now there are several problems with this:
-    1. Where do we store M3U files?
-    2. If the user moves files, the path stored in the M3U file doesn't automatically change and
-    therefore causes issues.
+#EXTM3U
+
+#EXTINF:111, Sample artist name - Sample track title
+C:\Music\SampleMusic.mp3
+
+#EXTINF:222,Example Artist name - Example track title
+C:\Music\ExampleMusic.mp3
+
+To know more about it, check out this link: https://docs.fileformat.com/audio/m3u/#extended-m3u
 */
 
 public class Playlist {
