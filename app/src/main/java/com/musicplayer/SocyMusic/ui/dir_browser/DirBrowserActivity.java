de335421a269035e2db45a848fba6fea407a5d88
Benji377 <demetzbenjamin23@gmail.com>
2021-08-17 20:34:37 +0200

    Fixed theme on DirBrowser
    I technically also added a new gitignore rule to avoid uploading of .idea files, but I am not so sure if it works as intended since it seems like I am still able to upload .idea files

--- app/src/main/java/com/musicplayer/SocyMusic/ui/dir_browser/DirBrowserActivity.java
+++ app/src/main/java/com/musicplayer/SocyMusic/ui/dir_browser/DirBrowserActivity.java
@@ -1,20 +1,70 @@
 package com.musicplayer.SocyMusic.ui.dir_browser;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.preference.PreferenceManager;
 
+import com.musicplayer.SocyMusic.SocyMusicApp;
 import com.musicplayer.musicplayer.R;
 
 
 public class DirBrowserActivity extends AppCompatActivity {
     private DirBrowserFragment dirBrowserFragment;
+    SharedPreferences.OnSharedPreferenceChangeListener listener;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String theme = prefs.getString(SocyMusicApp.PREFS_KEY_THEME, "Red_theme");
+        switch (theme) {
+            case "Red_theme":
+                setTheme(R.style.Theme_MusicPlayer);
+                break;
+            case "White_theme":
+                setTheme(R.style.WhiteTheme);
+                break;
+            case "Blue_theme":
+                setTheme(R.style.BlueTheme);
+                break;
+            case "Green_theme":
+                setTheme(R.style.GreenTheme);
+                break;
+            case "Orange_theme":
+                setTheme(R.style.OrangeTheme);
+                break;
+            case "Yellow_theme":
+                setTheme(R.style.YellowTheme);
+                break;
+            case "Aqua_theme":
+                setTheme(R.style.AquaTheme);
+                break;
+            case "Purple_theme":
+                setTheme(R.style.PurpleTheme);
+                break;
+            case "Purpleblue_theme":
+                setTheme(R.style.PurpleblueTheme);
+                break;
+            case "Orangegreen_theme":
+                setTheme(R.style.OrangegreenTheme);
+                break;
+            case "Purpleaqua_theme":
+                setTheme(R.style.PurpleaquaTheme);
+                break;
+        }
+        // Settings listener to update theme in realtime
+        // Use instance field for listener
+        // It will not be gc'd as long as this instance is kept referenced
+        listener = (prefs1, key) -> {
+            if (key.equals(SocyMusicApp.PREFS_KEY_THEME)) {
+                recreate();
+            }
+        };
+        prefs.registerOnSharedPreferenceChangeListener(listener);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dir_browser);
         setTitle(R.string.dir_browser_title);
