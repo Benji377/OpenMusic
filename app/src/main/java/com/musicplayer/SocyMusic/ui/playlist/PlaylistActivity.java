ebf4707185dd545a52ec3f9df1b50fab10ce7136
Benji377 <demetzbenjamin23@gmail.com>
2021-08-25 22:05:57 +0200

    Removed redundant code

--- app/src/main/java/com/musicplayer/SocyMusic/ui/playlist/PlaylistActivity.java
+++ app/src/main/java/com/musicplayer/SocyMusic/ui/playlist/PlaylistActivity.java
@@ -6,14 +6,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.preference.PreferenceManager;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.musicplayer.SocyMusic.SocyMusicApp;
 import com.musicplayer.SocyMusic.data.Playlist;
 import com.musicplayer.SocyMusic.data.Song;
