14302496f58df4ccaa7d2751fcd915078fb8e0ef
Benji <50681275+Benji377@users.noreply.github.com>
2021-09-04 16:38:25 +0200

    Minor changes
    I had to change the version of the database to two or it was giving me an error and crashing the app. I also increased the Sdkversion and added the proper icon to the settings

--- app/src/main/java/com/musicplayer/SocyMusic/data/base/AppDatabase.java
+++ app/src/main/java/com/musicplayer/SocyMusic/data/base/AppDatabase.java
@@ -8,7 +8,7 @@ import com.musicplayer.SocyMusic.data.Album;
 import com.musicplayer.SocyMusic.data.Playlist;
 import com.musicplayer.SocyMusic.data.Song;
 
-@Database(entities = {Song.class, Playlist.class, Album.class, PlaylistSong.class}, version = 1)
+@Database(entities = {Song.class, Playlist.class, Album.class, PlaylistSong.class}, version = 2)
 @TypeConverters({FileConverter.class, UUIDConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public static final String DATABASE_NAME = "socyMusic.sqlite3";
