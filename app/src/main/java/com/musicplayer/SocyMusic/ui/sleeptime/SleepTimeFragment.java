aa62b300a9178ce9f449389faf8df23c01b11df5
Benji <50681275+Benji377@users.noreply.github.com>
2021-09-18 18:11:07 +0200

    Added switch and description

--- app/src/main/java/com/musicplayer/SocyMusic/ui/sleeptime/SleepTimeFragment.java
+++ app/src/main/java/com/musicplayer/SocyMusic/ui/sleeptime/SleepTimeFragment.java
@@ -9,11 +9,13 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import com.musicplayer.musicplayer.R;
 
 public class SleepTimeFragment extends Fragment {
     private TimePicker timePicker;
+    private SwitchCompat switchCompat;
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
@@ -26,13 +28,14 @@ public class SleepTimeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sleeptime, container, false);
         timePicker = (TimePicker)view.findViewById(R.id.simpleTimePicker);
-        timePicker.setIs24HourView(true);
+        timePicker.setIs24HourView(false);
+        switchCompat = (SwitchCompat)view.findViewById(R.id.timePickerSwitch);
 
         timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
             public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
                 // display a toast with changed values of time picker
-                Toast.makeText(requireContext(), hourOfDay + "  " + minute, Toast.LENGTH_SHORT).show();
+                Toast.makeText(requireContext(), getCombined(), Toast.LENGTH_SHORT).show();
                 //time.setText("Time is :: " + hourOfDay + " : " + minute); // set the current time in text view
             }
         });
@@ -51,6 +54,22 @@ public class SleepTimeFragment extends Fragment {
     }
     public int getHour() {
         return timePicker.getHour();
+    }
+    public String getAmPm() {
+        return (getHour() < 12) ? "AM" : "PM";
+    }
+    public String getCombined() {
+        // Time in hh:mm:AA format
+        String ret = "";
+        ret+=getHour()+":";
+        ret+=getMinute()+":";
+        ret+=getAmPm();
+
+        return ret;
+    }
+
+    public boolean getSwitchState() {
+        return switchCompat.isChecked();
     }
 
     protected void onBackPressed() {
